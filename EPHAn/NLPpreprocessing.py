e3dec8ea06c6e5acf3be5748b952b24ed0ad393a
Ethan Irby <Ethan@ethans-mbp-5.myfiosgateway.com>
2021-05-19 15:37:48 -0400

    Added analysis notebookon reports.json

---
+++ EPHAn/NLPpreprocessing.py
@@ -0,0 +1,5 @@
+import nltk
+
+
+
+sample_text = ['I have a really hard time focusing and dont know if I am absorbing the information I read and learn on on a daily basis.']
--- EPHAn/erowidscrape.py
+++ EPHAn/erowidscrape.py
@@ -4,6 +4,7 @@ from urllib.request import urlopen as uReq
 from tqdm import tqdm
 from selenium.common.exceptions import NoSuchElementException
 import pandas as pd
+import os
 
 browser = webdriver.Chrome(executable_path='/Users/Ethan/Library/Python/3.7/lib/python/site-packages/selenium/chromedriver')
 '''
@@ -28,7 +29,7 @@ for parent in tqdm(list(substance_parents)):
 
 substance_df = pd.DataFrame(substance_dict)
 browser.quit()
-'''
+
 my_url = 'https://www.erowid.org/experiences/exp.php?ID=25991'
 browser.get(my_url)
 
@@ -64,7 +65,7 @@ for parent in tqdm(list(report_parent2)):
             ### print(quote.text) # prints all quotes
     except NoSuchElementException as e:
         pass
-
+'''
 ## grab all the text (citation, dose, body weight, up until date published and views, )
 ## export the text into a .txt file
 
@@ -75,5 +76,88 @@ main_url = 'https://www.erowid.org/experiences/exp_list.shtml'
 #substance_parents = browser.find_elements_by_tag_name('b')[3:]
 #for parent in tqdm(list(substance_parents)):
     #try:
+
+
+# navigate to deepest iterator in CSV: Substance, Experience type, then data (list of all experience links, columns for each link, category, and drug name)
+# make folders for each substance inside of which i make a folder for each experience type
+# and make files inside that are named by their ID
+# os.path.exists: then makedires
+# os makedirs export text file into a directory
+
+'''        if row == substance_name:
+            for substance in substance_name:
+                if substance != substance + i:
+                    substance_link = 'https://www.erowid.org/experiences/' + substance_name
+                    browser.get(substance_link)'''
+
+data_dir = '/Users/Ethan/Documents/Documents/Documents - Ethan’s MacBook Pro/GitHub/ErowidScraper/Orreen/data'
+substance_path = data_dir + '/substance_text_folder'
+exp_type_path = substance_path + '/experience_types_text_folder'
+report_file = 'exp_type_text.txt'
+
+
+def create_folders():
+    experience_types_csv = pd.read_csv('experience_types.csv')
+    experience_links_csv = pd.read_csv('experience_links.csv')
+    for i, row in tqdm(list(experience_links_csv.iterrows())):
+        substance_name = row['substance_name']
+        # start by substance then go to experience type
+        if not os.path.exists(substance_path):
+            for sub_folder in substance_name:
+                substance_folders = os.mdir(os.path.join(substance_path, sub_folder))
+                for j, roww in tqdm(list(experience_types_csv.iterrows())):
+                    experience_type = roww['experience_type']
+                    if not os.path.exists(exp_type_path):
+                        experience_type_links = roww['experience_type_link']
+                        for exp_type in list(experience_type): # create sub_folder for each experience
+                            experience_type_folders = os.mkdir(os.path.join(exp_type_path, exp_type))
+                            return substance_folders, experience_type_folders
+
+
+create_folders = create_folders()
+
+
+def get_exp_reports():
+    save_path = exp_type_path
+    for folder in exp_type_path: # the directory
+        for file in folder: # write files to each folder
+            file_exists = os.path.isfile(file)
+            for i, row in tqdm(list(experience_types_csv.iterrows())):
+                experience_type_link = row['experience_type_link']
+                full_exp_type_link = 'https://www.erowid.org/experiences/subs/' + experience_type_link
+                browser.get(full_exp_type_link)
+                report_parent_1 = browser.find_elements_by_tag_name('div')
+                for parent1 in tqdm(list(report_parent_1)):
+                    try:
+                        report_title = parent1.find_element_by_class_name("title")
+                        report_substance = parent1.find_element_by_class_name("substance")
+                        author = parent1.find_element_by_class_name("author")
+                        citation = parent1.find_element_by_class_name("ts-citation")
+                    except NoSuchElementException as E:
+                        pass
+
+                report_parent_2 = browser.find_elements_by_class_name("report-text-surround")
+                for parent2 in tqdm(list(report_parent_2)):
+                    qs = parent2.find_elements_by_class_name("pullquote-right1")
+                    try:
+                        dose_chart = parent2.find_element_by_class_name("dosechart").text
+                        body_weight = parent2.find_element_by_class_name("bodyweight").text
+                        trip_txt = parent2.text
+                        trip_txt = '\n'.join(trip_txt.split('\n')[:-2])
+                        for q in qs:
+                            if q.text in trip_txt:
+                                trip_txt = trip_txt.replace(q.text, '', 1)
+                                # print(q.text)
+                        # print(trip_txt)
+                    except NoSuchElementException as E:
+                        pass
+
+                file_name = report_title
+                if not file_exists:
+                    txt_file = os.path.join(save_path, file_name+".txt")
+                    with open(txt_file, 'w') as text_file:
+                        write_txt_files = text_file.write(trip_txt)
+                        write_txt_files.close()
+
 input()
 browser.quit()
